5b5c82262061e0bd7ec6829ec088642611c69ab7
dddaisuke <dddaisuke@gmail.com>
2024-02-16 11:13:07 +0900

    Azure Prompt Flowを追加

--- popup.js
+++ popup.js
@@ -21,7 +21,8 @@ function updateIcon() {
         url.startsWith("https://www.phind.com") ||
         url.startsWith("https://bard.google.com") ||
         url.startsWith("https://www.chatpdf.com") ||
-        url.startsWith("https://www.perplexity.ai")) {
+        url.startsWith("https://www.perplexity.ai") ||
+        url.startsWith("https://ml.azure.com/prompts/flow/")) {
       chrome.action.setIcon({ path: isEnabled ? "icon/enabled.png" : "icon/disabled.png" });
     }
   });
